7af9ba43a5155e3f6ac45bec608b304cdc1f84a7
chennqqi <chennqqi@qq.com>
2019-07-30 19:44:12 +0800

    add support for quay.io
    Signed-off-by: chennqqi <chennqqi@qq.com>

--- docker_wrapper.py
+++ docker_wrapper.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 # coding=utf8
+# utf8 without BOM
 
 import os
 import sys
 
-mirror = "gcr.azk8s.cn"
-namespace = "google_containers"
-prefix = "gcr.io"
-specialPrefix = "k8s.gcr.io"
+#  gcr.io/xxx/yyy:zzz -> gcr.azk8s.cn/xxx/yyy:zzz
+#  k8s.gcr.io/xxx:yyy => gcr.io/google-containers/xxx:yyy -> gcr.azk8s.cn/google-containers/xxx:yyy
+#  quay.io/xxx/yyy:zzz -> quay.azk8s.cn/xxx/yyy:zzz
+
+converts = [
+    {
+        'prefix': 'gcr.io',
+        'replace': lambda x: x.replace('gcr.io', 'gcr.azk8s.cn'),
+    },
+    {
+        'prefix': 'k8s.gcr.io',
+        'replace': lambda x: x.replace('k8s.gcr.io', 'gcr.azk8s.cn/google-containers'),
+    },
+    {
+        'prefix': 'quay.io',
+        'replace': lambda x: x.replace('quay.io', 'quay.azk8s.cn'),
+    }
+]
 
 def execute_sys_cmd(cmd):
     result = os.system(cmd)
@@ -23,31 +38,29 @@ if __name__ == "__main__":
         usage()
         sys.exit(-1)
 
-    # image name like k8s.gcr.io/kube-apiserver:v1.14.1 or gcr.io/google_containers/kube-apiserver:v1.14.1
     image = sys.argv[2]
     imageArray = image.split("/")
-    
-    if imageArray[0] != prefix and imageArray[0] != specialPrefix:
+   
+    newImage = '' 
+    for cvt in converts:
+        if imageArray[0] == cvt['prefix']:
+    	      # image name like k8s.gcr.io/kube-apiserver:v1.14.1 or gcr.io/google_containers/kube-apiserver:v1.14.1        	
+            newImage = cvt['replace'](image)
+            break
+    if newImage:
+        print("-- pull {image} from {newimage} instead --".format(image=image, newimage=newImage))
+        cmd = "docker pull {image}".format(image=newImage)
+        execute_sys_cmd(cmd)
+     
+        cmd = "docker tag {newImage} {image}".format(newImage=newImage, image=image)
+        execute_sys_cmd(cmd)
+       
+        cmd = "docker rmi {newImage}".format(newImage=newImage)
+        execute_sys_cmd(cmd)
+       
+        print("-- pull {image} done --".format(image=image))
+        sys.exit(0)
+    else:
         cmd = "docker pull {image}".format(image=image)
         execute_sys_cmd(cmd)
         sys.exit(0)
-
-    if len(imageArray) == 2:
-        seq = (mirror, namespace, imageArray[1])
-    else:
-        seq = (mirror, imageArray[1], imageArray[2])
-
-    newImage = "/".join(seq)
-
-    print("-- pull {image} from gcr.azk8s.cn instead --".format(image=image))
-    cmd = "docker pull {image}".format(image=newImage)
-    execute_sys_cmd(cmd)
-
-    cmd = "docker tag {newImage} {image}".format(newImage=newImage, image=image)
-    execute_sys_cmd(cmd)
-
-    cmd = "docker rmi {newImage}".format(newImage=newImage)
-    execute_sys_cmd(cmd)
-
-    print("-- pull {image} done --".format(image=image))
-    sys.exit(0)
